8e90cfbfee78486e92171f9617eba57e43ac210c
evezheng <evelynzhengfencing@gmail.com>
2024-10-09 22:09:28 -0400

    photos-1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,9 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import defaultimage from "../assests/image-icon-600nw-211642900.webp";
 import { Link } from "react-router-dom";
+import fencingimg from "../assests/OFAChampionships_617.jpg";
+import speakingimg from "../assests/rbcolympians.jpg";
+import modellingimg from "../assests/EZ111N8736.JPG";
 
 const HomePage = () => {
   return (
@@ -15,19 +18,19 @@ const HomePage = () => {
       <div className="grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 lg:gap-2 px-10 md:px-16 lg:px-24 justify-items-center py-16">
         <div>
           <Link to="/fencing" className="hover:bg-opacity-35">
-            <img src={defaultimage} alt="/" className="w-full" />
+            <img src={fencingimg} alt="/" className="w-full" />
           </Link>
           <h2 className="w-full py-4 text-4xl font-cormorant">Fencing</h2>
         </div>
         <div>
           <Link to="/speaking" className="hover:bg-opacity-35">
-            <img src={defaultimage} alt="/" className="w-full" />
+            <img src={speakingimg} alt="/" className="w-full" />
           </Link>
           <h2 className="w-full py-4 text-4xl font-cormorant">Speaking</h2>
         </div>
         <div>
           <Link to="/modelling" className="hover:bg-opacity-35">
-            <img src={defaultimage} alt="/" className="w-full" />
+            <img src={modellingimg} alt="/" className="w-full" />
           </Link>
           <h2 className="w-full py-4 text-4xl font-cormorant">Modelling</h2>
         </div>
